cd987bd8b88a32ddfa3b16bda73a4f4a29b9d5cc
fadiabuhanna <84229883+fadiabuhanna@users.noreply.github.com>
2022-06-24 23:42:52 +0300

    Update Try.java

--- Try.java
+++ Try.java
@@ -1,8 +1,30 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 
-class Try {
-    public static void main(String[] args) {
-        System.out.println("Hello, Fadi!"); 
-    }
+import java.io.File; 
+
+public class Try 
+{
+	static void func(String folderName){
+		
+		try{
+			File folder = new File(folderName);
+			File[] files = folder.listFiles();
+			
+			for (File file : files) {
+
+					System.out.println(file.getName());
+					if(file.isDirectory())
+						func(file.getAbsolutePath());
+			}
+		}
+		catch(Exception e){
+			System.out.println("this folder: " + folderName + " not empty!");
+		}
+		
+		
+	}
+	
+  public static void main(String[] args) {
+    func("test");
+  }
+  
 }
