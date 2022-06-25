14fa060d4cb81b3672540f65210e9f63ed43a7d4
fadiabuhanna <84229883+fadiabuhanna@users.noreply.github.com>
2022-06-25 15:51:18 +0300

    Update Try.java

--- Try.java
+++ Try.java
@@ -23,7 +23,7 @@ public class Try
 		
 	}
 	
-  public static void main(String[] args) 
+  public static void main(String[] args){
 	System.out.println(System.getProperty("user.dir"));
 	//readFolder(args[0]);
   }
