8e48d5b814926f3908c42ab6eec82c9947744ff9
fadiabuhanna <84229883+fadiabuhanna@users.noreply.github.com>
2022-07-01 21:05:06 +0300

    Update Try.java

--- Try.java
+++ Try.java
@@ -1,38 +1,41 @@
 
 
-import java.io.InputStream;
+//This class implements an output stream in which the data is written into a byte array
 import java.io.ByteArrayOutputStream;
-import com.jcraft.jsch.ChannelSftp;
+
 import com.jcraft.jsch.*;
 
 
 
-public class Try 
+public class PrintDirectory 
 {
 
-	static void path(String SFTPUSER, String SFTPHOST, int SFTPPOTR) {
+	static void path(String USER, String HOST, int POTR) {
 		Session session = null;
 		ChannelExec channel = null;
 		try{
 			//session = new JSch().getSession("fadi", "127.0.0.1", 2222);
-			session = new JSch().getSession(SFTPUSER, SFTPHOST, SFTPPOTR);
+			session = new JSch().getSession(USER, HOST, POTR);
 			session.setPassword("123123123");
 			session.setConfig("StrictHostKeyChecking", "no");
 			session.connect();
 			
 			channel = (ChannelExec) session.openChannel("exec");
-			channel.setCommand("ls -R");
+			
+			// ls -R: Takes all folders and files (Recursive)
+			channel.setCommand("ls -R"); 
 			ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
 			channel.setOutputStream(responseStream);
 			channel.connect();
 			
+			//Have to wait a few seconds to fetch the folders and files
 			while (channel.isConnected()) {
 				Thread.sleep(100);
 			}
 			
 			String responseString = new String(responseStream.toByteArray());
 
-			
+			// print the list of content - folders and files
 			System.out.println(new String(responseStream.toByteArray()));
 			
 			session.disconnect(); //stop
@@ -47,15 +50,14 @@ public class Try
 	public static void main(String[] args){
 		
 		 try{
-            String SFTPUSER="fadi";
-			String SFTPHOST="127.0.0.1";
-			int SFTPPOTR = Integer.parseInt("2222");
-			path(SFTPUSER,SFTPHOST,SFTPPOTR);
+            String USER=args[0];
+			String HOST=args[1];
+			int POTR = Integer.parseInt(args[2]);
+			path(USER,HOST,POTR);
         }
         catch (NumberFormatException ex){
             ex.printStackTrace();
         }
 	}
 }
-
 
