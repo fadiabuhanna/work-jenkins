be8d7e0ac74762dca6e14be0fa4cab082f2ec19c
fadiabuhanna <84229883+fadiabuhanna@users.noreply.github.com>
2022-07-02 18:03:15 +0300

    Update Try.java

--- Try.java
+++ Try.java
@@ -26,7 +26,7 @@ public class PrintDirectory
 			channel = (ChannelExec) session.openChannel("exec");
 			
 			// ls -R: Takes all folders and files (Recursive)
-			if(ROOT == ""){ channel.setCommand("ls"); System.out.println("HELLO");}
+			if(ROOT == ""){  System.out.println("HELLO"); channel.setCommand("ls");}
 			else channel.setCommand("ls "+ ROOT);
 			ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
 			channel.setOutputStream(responseStream);
